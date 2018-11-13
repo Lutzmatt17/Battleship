96d6a9db19001e6b73dfbae8018b468565059477
bctownsend829 <bc_townsend@outlook.com>
2018-11-13 17:23:49 -0500

    Grid print.

--- src/server/BattleShipDriver.java
+++ src/server/BattleShipDriver.java
@@ -3,12 +3,8 @@ package server;
 public class BattleShipDriver {
     public static void main(String[] args){
         Grid grid = new Grid();
-        String grid2[][] = grid.makeGrid();
-        for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
-                System.out.print(grid2[i][j]);
-            }
-        }
-
+        grid.makeGrid();
+        System.out.println(grid.displayOwnerGrid());
+        System.out.println(grid.displayRivalGrid());
     }
 }
--- src/server/Grid.java
+++ src/server/Grid.java
@@ -7,22 +7,63 @@ public class Grid {
     private static final String SUBMARINE = "S";
     private static final String CRUISER = "R";
     private static final String DESTROYER = "D";
+    private static final String HIT = "@";
+    private static final String MISS = "X";
     private static final int SIZE = 10;
 
     public Grid(){
         this.grid = new String[SIZE][SIZE];
     }
 
-    public String[][] makeGrid(){
-        String gridPiece1 = "+---+";
-        String gridPiece2 = "|\t|";
-        String subGrid = gridPiece1 + gridPiece2 + "\n" + gridPiece1;
-        for(int i = 0; i < 10; i ++){
-            for(int j = 0; j < 10; j++){
-                grid[i][j] = subGrid;
-            }
+    public void makeGrid(){
+
+    }
+
+    public String displayOwnerGrid() {
+        String sepLine = "  +---+---+---+---+---+---+---+---+---+---+";
+        StringBuilder build = new StringBuilder();
+        for(int i = 0; i < this.grid.length; i++) {
+            build.append("\t");
+            build.append(i);
         }
-        System.out.println(grid);
-        return grid;
+        build.append("\n");
+        build.append(sepLine);
+        for(int row = 0; row < this.grid.length; row++) {
+            build.append("\n");
+            build.append(row);
+            for(int col = 0; col < this.grid[row].length; col++) {
+                build.append(" | ");
+                //todo check if it is our player or not
+                build.append(grid[row][col]);
+            }
+            build.append(" |");
+            build.append("\n");
+            build.append(sepLine);
+        }
+        return build.toString();
+    }
+
+    public String displayRivalGrid() {
+        String sepLine = "  +---+---+---+---+---+---+---+---+---+---+";
+        StringBuilder build = new StringBuilder();
+        for(int i = 0; i < this.grid.length; i++) {
+            build.append("\t");
+            build.append(i);
+        }
+        build.append("\n");
+        build.append(sepLine);
+        for(int row = 0; row < this.grid.length; row++) {
+            build.append("\n");
+            build.append(row);
+            for(int col = 0; col < this.grid[row].length; col++) {
+                build.append(" | ");
+                //todo check if it is our player or not
+                build.append(grid[row][col]);
+            }
+            build.append(" |");
+            build.append("\n");
+            build.append(sepLine);
+        }
+        return build.toString();
     }
 }
