2a034ce0a1e4a14994b93825f65b729afa31e0d2
lutzmatt17 <lutzmatt17@gmail.com>
2018-11-13 16:33:01 -0500

    updates to grid

--- src/server/BattleShipDriver.java
+++ src/server/BattleShipDriver.java
@@ -1,4 +1,14 @@
 package server;
 
 public class BattleShipDriver {
+    public static void main(String[] args){
+        Grid grid = new Grid();
+        String grid2[][] = grid.makeGrid();
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                System.out.print(grid2[i][j]);
+            }
+        }
+
+    }
 }
--- src/server/Grid.java
+++ src/server/Grid.java
@@ -1,4 +1,28 @@
 package server;
 
 public class Grid {
+    private String[][] grid;
+    private static final String BATTLESHIP = "B";
+    private static final String CARRIER = "C";
+    private static final String SUBMARINE = "S";
+    private static final String CRUISER = "R";
+    private static final String DESTROYER = "D";
+    private static final int SIZE = 10;
+
+    public Grid(){
+        this.grid = new String[SIZE][SIZE];
+    }
+
+    public String[][] makeGrid(){
+        String gridPiece1 = "+---+";
+        String gridPiece2 = "|\t|";
+        String subGrid = gridPiece1 + gridPiece2 + "\n" + gridPiece1;
+        for(int i = 0; i < 10; i ++){
+            for(int j = 0; j < 10; j++){
+                grid[i][j] = subGrid;
+            }
+        }
+        System.out.println(grid);
+        return grid;
+    }
 }
